45c7b0d6f663e816c32e66125e5a2ac59a57a967
johipe <johipe@gmail.com>
2016-02-08 17:56:41 -0500

    gulp added

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+//adding required modules to the project
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+
+var debug = require('gulp-debug');
+var inject = require('gulp-inject');
+var tsc = require('gulp-typescript');
+var sourcemaps = require('gulp-sourcemaps');
+var tscProject = tsc.createProject('tsconfig.json');
+var connect = require('gulp-connect');
+var open = require('gulp-open');
+
+/*
+//example task
+gulp.task('example',function(){
+    gutil.log("Example started");
+    
+});
+*/
+// Declare file sources
+var TypeScriptSources = [
+    './Scripts/**/*.ts',
+    './typings/**/*.ts'];
+
+var HTMLSources = ['./**/*.html'];
+
+var CSSSources = ['./Content/**/*.css'];
+
+// This task Transpiles TypeScript to JavaScript
+gulp.task('transpile', function () {
+    gutil.log("transpiling...");
+
+    var tsResult = gulp.src(TypeScriptSources)
+        .pipe(sourcemaps.init())
+        .pipe(tsc(tscProject))
+        .on('error', gutil.log);
+
+    tsResult.dts.pipe(gulp.dest('./Scripts/'));
+    return tsResult.js
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./Scripts/'))
+        .on('error', gutil.log);
+
+});
+
+
